47f946b5bea899ac6e7c124db5848988f5bb14e4
mokshya17sahu <152842851+mokshya17sahu@users.noreply.github.com>
2023-12-30 21:23:54 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,8 @@
+document.querySelector(".button").addEventListener("click",handleclick)
+function handleclick() {
+    alert("You can directly contact us on  this number 7008660661");
+}
+document.querySelector(".xyz").addEventListener("click",clickclick)
+function clickclick() {
+    alert ("I am a passionate web developer and designer .I have basic knowledge on javascript,html,css,python and C. We aim to change the world of web development and designing .");
+}
